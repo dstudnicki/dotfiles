33a6351746fd412a438097ea0c04951f58e461a2
dstudnicki <domistu@poczta.fm>
2025-05-07 00:24:55 +0200

    add oh-my-zsh and powerlevel10k theme

--- install.sh
+++ install.sh
@@ -19,8 +19,11 @@ echo "===> [3/5] Copying Plasma themes, look-and-feel and widgets to ~/.local/sh
 mkdir -p ~/.local/share/plasma
 cp -rv local/share/plasma/* ~/.local/share/plasma/
 
-echo "===> [4/5] Installing ZSH configuration and oh-my-zsh..."
+echo "===> [4/5] Installing ZSH configuration and oh-my-zsh with powerlevel10k theme..."
 cp -v zshrc/.zshrc ~/.zshrc
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+cp powerlevel10k/.p10k.zsh ~/.p10k.zsh
 
 echo "===> [5/5] Copying Kitty terminal config..."
 mkdir -p ~/.config/kitty
